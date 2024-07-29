f6f8b27a787115332579ecc279e4b8cc184d2f52
soydev <admin@soyja.cc>
2024-07-29 18:09:45 -0400

    Update mod.php
    added ability to search for specific thread's password

--- mod.php
+++ mod.php
@@ -1,7 +1,10 @@
+<?php
+session_start();
+?>
 <html>
 
 <head>
-    <title>Crypt Api</title>
+    <title>Encrypt Mod Portal</title>
     <link rel="icon" type="image/x-icon" href="classic.png">
     <style>
         body {
@@ -32,11 +35,37 @@
             Pass <input type="password" name="pass"></input><br/>
             <input type="submit" name="submit" value="Go"></input>
             </form>
-            <a href = "index.html">Back</a>
+            <hr>
+                        <form method="POST" action="">
+            <input style = "display:none" type="text" name="log" id = "log2" value = "e" ></input>
+            Search <input type="text" name="term"></input>
+            <input type="submit" name="submit" value="Go"></input>
+            </form>
+            <div id = "result"></div>
             <hr>
    </body>
+   <script>
+       
+       function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      document.getElementById("log2").value = c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+getCookie("e");
+   </script>
 </html>
 <?php
+
 if($_SERVER["REQUEST_METHOD"] && isset($_POST["user"]) && isset($_POST["pass"])) {
     
     if($_POST["user"] == "admin" && $_POST["pass"] == "changeme"){
@@ -44,11 +73,33 @@ if($_SERVER["REQUEST_METHOD"] && isset($_POST["user"]) && isset($_POST["pass"]))
         $decode = json_decode($dcode,true);
         $link = array_column(array_reverse($decode), "link");
         $seed = array_column(array_reverse($decode), "pass");
+        // you can also replace the IV if you need.
+        setcookie('e', openssl_encrypt($_SERVER["REMOTE_ADDR"], "AES-128-CTR",
+            "[ENTER 2ND PASS]", 0, "0192034627925103"), time() + (86400 * 30), "/");
+
         for($i = 0; $i < count($link); $i++) {
             echo "<b>password: ".$seed[$i]." : ".$link[$i]."</b><br>";
             
         }
         echo "<hr>";
     }
+}
+if($_SERVER["REQUEST_METHOD"] && isset($_POST["log"]) && isset($_POST["term"])) {
+    
+    
+        if($_POST["log"] = openssl_decrypt($_COOKIE["e"], "AES-128-CTR",
+            "[ENTER 2ND PASS]", 0, "0192034627925103")) {
+                $dcode = file_get_contents("thread.json");
+        $decode = json_decode($dcode,true);
+        $link = array_column(array_reverse($decode), "link");
+        $seed = array_column(array_reverse($decode), "pass");
+        for($i = 0; $i < count($link); $i++) {
+            if($link[$i] = $_POST["term"]){
+                echo "<script>let p = document.createElement('p'); p.innerText = 'The password for that thread is: ".$seed[$i]."'; document.getElementById('result').appendChild(p);</script>";
+            }
+            
+        }
+            }
+
 }
 ?>
